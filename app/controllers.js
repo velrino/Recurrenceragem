9ca36a6dd4124a161a5be96becbc8b5abd523793
velrino <denismagalhaesvelrino@gmail.com>
2018-06-08 16:29:25 -0300

    Controllers managing with services and eventually with Models

--- app/controllers.js
+++ app/controllers.js
@@ -1,10 +1,29 @@
 import services from './services';
 
 const controllers = {
-    teste: {
-        init : function() {
+    plans: {
+        list : function() {
+            return services.PagarMe.request('1/plans')
+            .then(result => { console.log(result); return result; })
+            .catch(error => { console.error(error); throw error; });
+        },
+        create : function() {
+
+            services.PagarMe.request(
+                '1/plans',
+                'POST',
+                {
+                    amount: 150,
+                    days: 30,
+                    name: 'Plan Teste',
+                    payment_methods: ['boleto', 'credit_card']
+                }
+            )
+            .then(result => { console.log(result); return result; })
+            .catch(error => { console.error(error); throw error; });
+            
             return {
-                info: services.PagarMe.init()
+                status: 'created'
             };
         }
     }
